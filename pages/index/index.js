904c8770bb5e071eee8f07c5fcd33aa18eb2af6c
unknown <1048158743@qq.com>
2019-11-11 16:25:01 +0800

    02_小程序配置和页面配置

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,92 @@
-//index.js
-//获取应用实例
+//获取全局共享数据的方法
+//getApp()可获取App()产生的示例对象
 const app = getApp()
-
+const datas = app.globalData;
+console.log("共享数据：", datas)
 Page({
+//----------------------2.初始化数据-------------------------
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+//------------------------1.生命周期函数-----------------------
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  //---------------------------3.监听绑定事件----------------
+
+  /**
+   * 获取用户信息
+   */
+  handleGetUserInfo(event) {
+    //event.detail中存放的就是用户信息
+    console.log("用户信息：", event)
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+  // --------------------------4.监听其他事件------------------
+  //监听页面滚动
+  onPageScroll(obj) {
+    console.log('滚动事件--：',obj)
+  },
+  //监听页面是否滚动到底部
+  onReachBottom(obj){
+    console.log("页面滚动到底部")
+  },
+  //下拉刷新
+  onPullDownRefresh(){
+    console.log("下拉刷新")
   }
+
 })
