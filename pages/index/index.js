40cc5f321b9b3c1a2089e87717fda363e0768121
unknown <1048158743@qq.com>
2019-11-13 11:08:52 +0800

    05_事件处理

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,112 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    arr0:["春花","秋月","冷杨"]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  handleconsole() {
+    console.log("打印")
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  handletouchstart(){
+    console.log("手指开始触摸")
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  handletouchmove(){
+    console.log("触摸移动手势")
+  },
+  handletouchcancel(){
+    console.log("触摸取消手势")
+  },
+  handletouchend(){
+    console.log("触摸结束手势")
+  },
+  handletap(){
+    console.log("轻点手势")
+  },
+  handlelongtap(){
+    console.log("长按手势")
+  },
+  handleOuter(event){
+    console.log('outer---:',event)
+  },
+  handleInner(event){
+    console.log('inner---:',event)
+  },
+  handleTap0(event){
+    console.log("事件参数传递",event)
+  },
+  handlecaptureview1(event){
+    console.log("handlecaptureview1",event)
+  },
+  handleview1(event){
+    console.log("handleview1",event)
+  },
+  handlecaptureview2(event){
+    console.log("handlecaptureview2",event)
+  },
+  handleview2(event){
+    console.log("handleview2",event)
+  },
+  handlecaptureview3(event){
+    console.log("handlecaptureview3",event)
+  },
+  handleview3(event){
+    console.log("handleview3",event)
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
   }
 })
