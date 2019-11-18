65ea5aadfe3492bcfe1823b2d38fc1fa9dd74c1c
unknown <1048158743@qq.com>
2019-11-18 11:36:22 +0800

    09_navigator使用

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,65 +1,76 @@
+// pages/detail/detail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    name:"唐小七"
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+    console.log('options', options)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+    //1.获取首页的页面对象
+    //getCurrentPage()获取当前所有栈的页面
+
+    const pages=getCurrentPages()
+    const home=pages[pages.length-2]
+    //2.调用页面对象的setdata
+    home.setData({
+      name:"小七仙女"
+    })
+   
+   console.log("pages",pages)
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
   }
 })
